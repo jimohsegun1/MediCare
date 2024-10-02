a26a73a304fb7995ba3a942a66b5ad04dabebe29
jimohsegun1 <jimohsegunj@gmail.com>
2024-10-02 15:06:27 +0100

    backend: add-doctor

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,20 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
-connectDB()
-connectCloudinary()
+connectDB();
+connectCloudinary();
 
 // middlewares
 app.use(express.json());
 app.use(cors());
 
 // api endpoints
+app.use("/api/admin", adminRouter); // loalhost:4000/api/admin/add-doctor
 
 app.get("/", (req, res) => {
   res.send("API working correctly");
