4ef202a6645c76d7d7d4dc3c4a58cb291379769f
jimohsegun1 <jimohsegunj@gmail.com>
2024-10-05 21:46:06 +0100

    backend: login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,4 +36,23 @@ const registerUser = async (req, res) => {
   }
 };
 
-export {registerUser}
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      console.log(error);
+      res.json({ success: false, message: "user does not exist" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else res.json({ success: false, message: "invalid credentials" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser, loginUser };
