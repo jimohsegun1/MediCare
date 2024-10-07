f1c2d6a342141e77780e61b31319d14349a42f92
jimohsegun1 <jimohsegunj@gmail.com>
2024-10-07 13:36:23 +0100

    frontend: setup AooContext for profile

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -13,6 +13,7 @@ const AppContextProvider = (props) => {
   const [token, setToken] = useState(
     localStorage.getItem("token") ? localStorage.getItem("token") : false
   );
+  const [userData, setUserData] = useState(false)
 
   const getDoctorsData = async (params) => {
     try {
@@ -28,17 +29,42 @@ const AppContextProvider = (props) => {
     }
   };
 
+  const loadUserProfileData = async (req, res) => {
+    try {
+      const { data } = await axios.get(backendUrl + '/api/user/get-profile', { headers: { token } })
+      if (data.success) {
+        setUserData(data.userData)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  }
+
   const value = {
     currencySymbol,
     doctors,
     token,
     setToken,
     backendUrl,
+    userData,
+    setUserData,
+    loadUserProfileData
   };
 
   useEffect(() => {
     getDoctorsData();
   }, []);
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData()
+    } else {
+      setUserData(false)
+    }
+  }, [token])
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
