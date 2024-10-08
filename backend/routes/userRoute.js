e7263e4973247a6b7982a1b17c38cbc456c09a9b
jimohsegun1 <jimohsegunj@gmail.com>
2024-10-08 02:45:20 +0100

    backend: book appointment

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  bookAppointment,
   getProfile,
   loginUser,
   registerUser,
@@ -14,6 +15,13 @@ userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 
 userRouter.get("/get-profile", authUser, getProfile);
-userRouter.post("/update-profile",upload.single('image'), authUser, updateProfile);
+userRouter.post(
+  "/update-profile",
+  upload.single("image"),
+  authUser,
+  updateProfile
+);
+
+userRouter.post("/book-appointment", authUser, bookAppointment);
 
 export default userRouter;
